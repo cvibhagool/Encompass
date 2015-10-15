976a7ef8830f41745966ca4ee2b0df4e1499a041
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-15 16:36:02 -0700

    Refactor Hello World in separate .js file loaded into index.html

--- client/public/app.js
+++ client/public/app.js
@@ -1,24 +1,6 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var HelloWorld = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//         Hello World!
-//       </div>
-//     )
-//   }
-// });
-
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('app');
-// );
-// React.render(<HelloWorld />, document.getElementById('app'));
-
 'use strict';
 
-React.render(React.createElement(
+ReactDOM.render(React.createElement(
   'h1',
   null,
   'Hello, world!'
--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ var express = require('express');
 //Creates and initializes an express application
 var app = express();
 var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Sever started on port", port);
 
 //Passes app and express to middlewares
 require('./config/middleware.js')(app, express);
+
+app.use(express.static(__dirname + "/../client/public"));
+app.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+app.listen(port);
+console.log("Sever started on port", port);

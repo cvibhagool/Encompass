aaeceb560db3c13a5300769f06bca0434177b2d7
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-15 17:42:45 -0700

    Implement basic navigation pane in unordered list format

--- client/public/app.js
+++ client/public/app.js
@@ -1,7 +1,47 @@
 'use strict';
 
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, world!'
-), document.getElementById('example'));
+var tabList = [{ 'id': 1, 'name': 'Mike', 'url': '/mike' }, { 'id': 2, 'name': 'Donnie', 'url': '/donnie' }, { 'id': 3, 'name': 'Raph', 'url': '/raph' }, { 'id': 4, 'name': 'Leo', 'url': '/leo' }];
+
+var NavBar = React.createClass({
+	displayName: 'NavBar',
+
+	render: function render() {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(Tabs, { tablist: tabList })
+		);
+	}
+});
+
+var Tab = React.createClass({
+	displayName: 'Tab',
+
+	render: function render() {
+		return React.createElement(
+			'li',
+			null,
+			React.createElement(
+				'a',
+				{ href: this.props.url },
+				this.props.name
+			)
+		);
+	}
+});
+
+var Tabs = React.createClass({
+	displayName: 'Tabs',
+
+	render: function render() {
+		return React.createElement(
+			'ul',
+			null,
+			this.props.tablist.map(function (tab) {
+				return React.createElement(Tab, { url: tab.url, name: tab.name });
+			})
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(NavBar, null), document.getElementById('example'));
--- client/src/app.js
+++ client/src/app.js
@@ -1,22 +1,40 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var HelloWorld = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//         Hello World!
-//       </div>
-//     )
-//   }
-// });
+var tabList = [
+    { 'id': 1, 'name': 'Mike', 'url': '/mike' },
+    { 'id': 2, 'name': 'Donnie', 'url': '/donnie' },
+    { 'id': 3, 'name': 'Raph', 'url': '/raph' },
+    { 'id': 4, 'name': 'Leo', 'url': '/leo' }
+];
 
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('app');
-// );
-// React.render(<HelloWorld />, document.getElementById('app'));
+var NavBar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Tabs tablist={tabList} />
+			</div>
+		);
+	}
+});
 
-React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+var Tab = React.createClass({
+	render: function() {
+		return (<li><a href={this.props.url}>{this.props.name}</a></li>);
+	}
+});
+
+var Tabs = React.createClass({
+	render: function(){
+		    return (
+	            <ul>
+	                {this.props.tablist.map(function(tab) {
+	                    return (
+	                        <Tab url={tab.url} name={tab.name} />
+	                    );
+	                })}
+	            </ul>
+	        );
+	    
+    }
+});
+
+ReactDOM.render(<NavBar/>, document.getElementById('example'));
+

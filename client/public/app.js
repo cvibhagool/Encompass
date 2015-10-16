080a485bb2769fb6937bbd4598064b73522d3401
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-15 19:38:53 -0700

    adds handleClick to Tabs

--- client/public/app.js
+++ client/public/app.js
@@ -1,68 +1,76 @@
 'use strict';
 
-var tabList = [{ 'id': 1, 'name': 'Home', 'url': '/home' }, { 'id': 2, 'name': 'Company View', 'url': '/company' }, { 'id': 3, 'name': 'Search', 'url': '/search' }, { 'id': 4, 'name': 'Compensation Packages', 'url': '/compensation' }];
+var tabList = [{ 'id': 1, 'name': 'Home', 'url': '/#/home' }, { 'id': 2, 'name': 'Company View', 'url': '/#/company' }, { 'id': 3, 'name': 'Search', 'url': '/#/search' }, { 'id': 4, 'name': 'Compensation Packages', 'url': '/#/compensation' }];
 
 var AppView = React.createClass({
-	displayName: 'AppView',
+  displayName: 'AppView',
 
-	render: function render() {
-		return React.createElement(
-			'div',
-			{ id: 'app-view' },
-			React.createElement(NavBar, { tablist: tabList }),
-			React.createElement(ContentView, null)
-		);
-	}
+  render: function render() {
+    return React.createElement(
+      'div',
+      { id: 'app-view' },
+      React.createElement(NavBar, { tablist: tabList }),
+      React.createElement(ContentView, null)
+    );
+  }
 });
 
 var NavBar = React.createClass({
-	displayName: 'NavBar',
+  displayName: 'NavBar',
 
-	render: function render() {
-		return React.createElement(
-			'div',
-			{ id: 'nav-bar' },
-			React.createElement(Tabs, { tablist: this.props.tablist })
-		);
-	}
+  render: function render() {
+    return React.createElement(
+      'div',
+      { id: 'nav-bar' },
+      React.createElement(Tabs, { tablist: this.props.tablist })
+    );
+  }
 });
 
 var Tabs = React.createClass({
-	displayName: 'Tabs',
+  displayName: 'Tabs',
 
-	render: function render() {
-		return React.createElement(
-			'ul',
-			null,
-			this.props.tablist.map(function (tab) {
-				return React.createElement(Tab, { key: tab.id, url: tab.url, name: tab.name });
-			})
-		);
-	}
+  handleClick: function handleClick() {
+    this.props.changeTab(tab);
+  },
+
+  render: function render() {
+    return React.createElement(
+      'ul',
+      null,
+      this.props.tablist.map((function (tab) {
+        return React.createElement(Tab, {
+          key: tab.id,
+          url: tab.url,
+          handleClick: this.handleClick.bind(this, tab),
+          name: tab.name });
+      }).bind(this))
+    );
+  }
 });
 
 var Tab = React.createClass({
-	displayName: 'Tab',
+  displayName: 'Tab',
 
-	render: function render() {
-		return React.createElement(
-			'li',
-			{ className: 'tab' },
-			React.createElement(
-				'a',
-				{ href: this.props.url },
-				this.props.name
-			)
-		);
-	}
+  render: function render() {
+    return React.createElement(
+      'li',
+      { className: 'tab' },
+      React.createElement(
+        'a',
+        { href: this.props.url },
+        this.props.name
+      )
+    );
+  }
 });
 
 var ContentView = React.createClass({
-	displayName: 'ContentView',
+  displayName: 'ContentView',
 
-	render: function render() {
-		return React.createElement('div', { id: 'content-view' });
-	}
+  render: function render() {
+    return React.createElement('div', { id: 'content-view' });
+  }
 });
 
 ReactDOM.render(React.createElement(AppView, null), document.getElementById('app'));
--- client/src/app.js
+++ client/src/app.js
@@ -1,60 +1,69 @@
 var tabList = [
-    { 'id': 1, 'name': 'Home', 'url': '/home' },
-    { 'id': 2, 'name': 'Company View', 'url': '/company' },
-    { 'id': 3, 'name': 'Search', 'url': '/search' },
-    { 'id': 4, 'name': 'Compensation Packages', 'url': '/compensation' }
+  { 'id': 1, 'name': 'Home', 'url': '/#/home' },
+  { 'id': 2, 'name': 'Company View', 'url': '/#/company' },
+  { 'id': 3, 'name': 'Search', 'url': '/#/search' },
+  { 'id': 4, 'name': 'Compensation Packages', 'url': '/#/compensation' }
 ];
 
 var AppView = React.createClass({
-	render: function() {
-		return (
-			<div id='app-view'>
-				<NavBar tablist={tabList} />
-				<ContentView/>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div id='app-view'>
+        <NavBar tablist={tabList} />
+        <ContentView/>
+      </div>
+    );
+  }
 })
 
 var NavBar = React.createClass({
-	render: function() {
-		return (
-			<div id='nav-bar'>
-				<Tabs tablist={this.props.tablist} />
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div id='nav-bar'>
+        <Tabs tablist={this.props.tablist} />
+      </div>
+    );
+  }
 });
-
 
 var Tabs = React.createClass({
-	render: function(){
-	    return (
-            <ul>
-                {this.props.tablist.map(function(tab) {
-                    return (
-                        <Tab key={tab.id} url={tab.url} name={tab.name} />
-                    );
-                })}
-            </ul>
-        );    
-    }
-});
 
+  handleClick: function() {
+    this.props.changeTab(tab)
+  },
+  
+  render: function() {
+    return (
+      <ul>
+        { this.props.tablist.map(function(tab) {
+          return (
+            <Tab 
+              key={tab.id} 
+              url={tab.url} 
+              handleClick={this.handleClick.bind(this, tab)} 
+              name={tab.name} />
+          );
+        }.bind(this))
+      }
+      </ul>
+    );
+  }
+});
+        
 
 var Tab = React.createClass({
-	render: function() {
-		return (<li  className='tab'><a href={this.props.url}>{this.props.name}</a></li>);
-	}
+  render: function() {
+    return (<li  className='tab'><a href={this.props.url}>{this.props.name}</a></li>);
+  }
 });
 
 var ContentView = React.createClass({
-	render: function() {
-		return (
-			<div id='content-view'>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div id='content-view'>
+      </div>
+    );
+  }
 });
 
 

7949856c340642e0024674f0817ebfdb05c05c77
Benjamin <bhoffman8@gmail.com>
2015-10-20 11:35:49 -0700

    refactoring into jquery

--- client/src/components/content/AddOffer.js
+++ client/src/components/content/AddOffer.js
@@ -1,85 +1,22 @@
 var AddOffer = React.createClass({
-
-  getInitialState: function() {
-    return {
-      type: 'info',
-      message: ''
-    }
-  },
-
-  // form submit callback
-  handleSubmit: function(e) {
-    e.preventDefault();
-
-    this.setState({ type: 'info', message: 'Sending...' }, this.sendFormData);
-  },
-
-  sendFormData: function() {
-
-    // prepare form data for submitting it
-    var formData = {
-      company_name: ReactDOM.findDOMNode(this.refs.budget).value,
-      position: ReactDOM.findDOMNode(this.refs.position).value,
-      salary: ReactDOM.findDOMNode(this.refs.salary).value,
-      equity: ReactDOM.findDOMNode(this.refs.equity).value,
-      vesting_start_date: ReactDOM.findDOMNode(this.refs.vesting_start_date).value,
-      vesting_end_date: ReactDOM.findDOMNode(this.refs.vesting_end_date).value,
-      vesting_cliff_date: ReactDOM.findDOMNode(this.refs.vesting_cliff_date).value,
-      vesting_cliff_percent: ReactDOM.findDOMNode(this.refs.vesting_cliff_percent).value,
-      other_benefits: ReactDOM.findDOMNode(this.refs.other_benefits).value,
-      last_financing_round_valuation: ReactDOM.findDOMNode(this.refs.last_financing_round_valuation).value,
-      estimated_eit_valuation: ReactDOM.findDOMNode(this.refs.estimated_eit_valuation).value
-    };
-
-    // extract the checkbox values
-    formData.benefits = this.getSelected('benefits');
-
-    // send the form data
-    var xmlhttp = new XMLHttpRequest();
-    var _this = this;
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === 4) {
-        var response = JSON.parse(xmlhttp.responseText);
-        if (xmlhttp === 200 && response.status === 'OK') {
-          _this.setState({ type: 'success', message: 'Offer submitted...'});
-        }
-        else {
-          _this.setState({ type: 'danger', message: 'Offer not submitted...'});
-        }
-      }
-    };
-    xmlhttp.open('POST', 'send', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send(this.requestBuildQueryString(formData));
-  },
-
-  requestBuildQueryString: function(params) {
-    var queryString = [];
-    for (var property in params) {
-      if (params.hasOwnProperty(property)) {
-        queryString.push(encodeURIComponent(property) + '=' + encodeURIComponent(params[property]));
-      }
-    }
-    return queryString.join('&');
-  },
-
-  getSelected: function(fieldName) {
-    var i;
-    var fields = document.getElementByName(fieldName);
-    var selectedFields = [];
-    for (i = 0; i < fields.length; i++) {
-      if (fields[i].checked === true) {
-        selectedFields.push(fields[i].value);
-      }
-    }
-    return selectedFields.join(', ');
+  
+  sendFormData: function(formData) {
+    $.ajax({
+      url: '/api/offer',
+      dataType: 'json',
+      type: 'POST',
+      data: formData,
+      success: function(data) {
+        console.log('Success!!!!')
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
 
   render: function() {
-    if (this.state.type && this.state.message) {
-      var classString = 'alert alert-' + this.state.type;
-      var status = <div id="status" className={classString} ref="status">{this.state.message}</div>
-    }
     return (
       <div>
         <h1 id="heading">Add Your Offer</h1>
@@ -150,110 +87,7 @@ var AddOffer = React.createClass({
   }
 });
 
-
-
-
-
-
-
-
-
-// POST request to server with all info listed above
-// update tables: users, offers, companies
-
-// GET request for metrics about the offer
-// so we can display the offer results on the page
-
-// var AddOffer = React.createClass({
-
-//   handleSubmit: function(e) {
-//     $.ajax({
-//       url: this.props.url,
-//       dataType: 'json',
-//       type: 'POST',
-//       data: comment, 
-//       success: function(data) {
-//         this.setState({data: data});
-//         console.log('POST')
-//       }.bind(this),
-//       error: function(xhr, status, err) {
-//         console.error(this.props.url, status, err.toString());
-//       }.bind(this)
-//     });
-
-//     e.preventDefault();
-//     var company = this.refs.company.value.trim();
-//     if (!company) {
-//       return;
-//     }
-
-//     this.refs.company.value = '';
-//     return;
-//   },
-
-//   render: function() {
-//     return (
-
-//       <form className="form-inline" onSubmit={this.handleSubmit}>
-//         <div className="form-group">
-//           <div>
-//             <label htmlFor="company">Startup:</label>
-//             <input type="text" className="form-control" id="company" placeholder="Which Startup?" ref="company" />
-//           </div>
-
-//           <label className="sr-only" htmlFor="salary">Salary (in dollars)</label>
-
-//           <div className="input-group">
-//             <div className="input-group-addon">$</div>
-//             <input type="number" className="form-control" id="salary" />
-//             <div className="input-group-addon">.00</div>
-//           </div>
-
-//           <label className="sr-only" htmlFor="equity">Equity Percentage</label>
-        
-//           <div className="input-group">
-//             <input type="number" className="form-control" id="equity" />
-//             <div className="input-group-addon">%</div>
-//           </div>
-
-//           Benefits: 
-//           <div className="checkbox">
-//             <label>
-//               <input type="checkbox" value="" /> Food
-//             </label>
-//             <label>
-//               <input type="checkbox" /> Healthcare
-//             </label>
-//           </div>
-
-//         </div>
-//         <button type="submit" className="btn btn-primary" value="Post">Submit Offer</button>
-//       </form>       
-//     )
-//   }
-// });
-
-// var Offers = React.createClass({
-//   getOffers: function() {
-//     $.ajax({
-//       url: this.props.url,
-//       dataType: 'json',
-//       cache: false,
-//       success: function(data) {
-//         this.setState({data: data});
-//       }.bind(this),
-//       error: function(xhr, status, err) {
-//         console.error(this.props.url, status, err.toString());
-//       }.bind(this)
-//     });
-//   }
-// });
-
-// var PostOffer = React.createClass()
-
-    // note: in our formula to calculate the value of the offer, we should also include federal and state taxes
-
-
 module.exports = {
   AddOffer: AddOffer
 }
+

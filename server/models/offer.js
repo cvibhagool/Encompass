e0ff956ac7cc41e2f05e77190bd6774460dd6f56
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-19 19:20:28 -0700

    Updated offer model

--- server/models/offer.js
+++ server/models/offer.js
@@ -1,10 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
   var Offer = sequelize.define('Offer', {
     position: {
-      type: DataTypes.STRING
+      type: Sequelize.STRING
     },
     salary: {
-      type: DataTypes.STRING
+      type: Sequelize.FLOAT
+    },
+    equity: {
+      type: Sequelize.FLOAT
+    },
+    vesting_start_date : {
+      type: Sequelize.DATEONLY
+    },
+    vesting_end_date : {
+      type: Sequelize.DATEONLY
+    },
+    vesting_cliff_date : {
+      type: Sequelize.DATEONLY
+    },
+    vesting_cliff_percent : {
+      type: Sequelize.FLOAT
+    },
+    other_benefits : {
+      type: Sequelize.FLOAT
+    },
+    last_financing_round_valuation : {
+      type: Sequelize.FLOAT
+    },
+    estimated_exit_valuation : {
+      type: Sequelize.FLOAT
     }
   }, {  
     classMethods: {

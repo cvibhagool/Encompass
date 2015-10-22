ae597bcba4299f23b7fd88f05c00f4c55c368a41
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-21 20:17:29 -0700

    Refactor client server to incorporate api server functionality

--- client/server.js
+++ client/server.js
@@ -3,12 +3,16 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
 
-var app = new require('express')();
-var port = 4000;
+var express = require('express');
+
+var app = express();
+var port = 3000;
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
+
+require('../server/config/middleware.js')(app, express);
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/index.html');

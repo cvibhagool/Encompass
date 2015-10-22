60775777d9677cc1d8ebb7c1d7774b9a3c7dd358
Aaron Norby <aaron.norby@gmail.com>
2015-10-22 15:50:17 -0700

    Add parcoords example for testing with React

--- server/frontend/components/Parallel.js
+++ server/frontend/components/Parallel.js
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react';
+import ReactDOM from 'react-dom';
 import d3 from 'd3';
 import * as parcoords from './lib/d3.parcoords.js';
 
@@ -14,6 +15,42 @@ export default class Parallel extends Component {
     ]}
   }
 
+  doD3(d3Node) {
+    console.log('special', d3Node);
+    // var pc = d3.parcoords()(d3Node)
+    //   .data(this.state.data)
+    //   .render()
+    //   .ticks(3)
+    //   .createAxes();
+    
+    d3.csv('nutrients.csv', function(data) {
+      var colorgen = d3.scale.ordinal()
+        .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
+            "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
+            "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
+
+      var color = function(d) { return colorgen(d.group); };
+
+      console.log('length: ', data.length);
+
+      var parcoords = d3.parcoords()(d3Node)
+        .data(data)
+        .hideAxis(["name"])
+        .color(color)
+        .alpha(0.25)
+        .composite("darken")
+        .margin({ top: 24, left: 150, bottom: 12, right: 0 })
+        .mode("queue")
+        .render()
+        .brushMode("1D-axes");  // enable brushing
+
+      parcoords.svg.selectAll("text")
+        .style("font", "10px sans-serif");
+      
+    });
+
+  }
+
   render() {
 
     var blue_to_brown = d3.scale.linear()
@@ -21,15 +58,17 @@ export default class Parallel extends Component {
       .range(['steelblue', 'brown'])
       .interpolate(d3.interpolateLab);
 
+    var divStyle = {width: "1300px", height: "270px"};
+
 
 
     return (
-      <div id="example">
+      <div>
         "hellow parcords"
+        <div style={divStyle} id="example" className="parcoords" ref={(ref) =>  this.doD3(ReactDOM.findDOMNode(ref))}></div>
+        
       </div>
     )
-
-    
 
   }
 

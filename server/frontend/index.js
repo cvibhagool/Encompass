9326b070eb03fd1496179353b434c87e7e5f3b21
Aaron Norby <aaron.norby@gmail.com>
2015-10-23 16:19:44 -0700

    Add in material-ui tabs

--- server/frontend/index.js
+++ server/frontend/index.js
@@ -4,10 +4,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './store/configureStore';
+let injectTapEventPlugin = require('react-tap-event-plugin');
 
 //Set the initial state and initialize the store with the initial state
 const initialState = {page: {currentPage: 1}};
 const store = configureStore(initialState);
+
+injectTapEventPlugin();
 
 render(
   <Provider store={store}>

05c718bee30a35839cd9ff450e51c09e04b27ec4
Aaron Norby <aaron.norby@gmail.com>
2015-10-23 17:17:52 -0700

    Add Card styling to landing page

--- server/frontend/components/Landing.js
+++ server/frontend/components/Landing.js
@@ -1,14 +1,18 @@
 import React, { PropTypes, Component } from 'react';
 import d3 from 'd3';
-//import * as parcoords from './lib/d3.parcoords.js';
+import { Card, CardHeader, CardText, CardTitle, Avatar } from 'material-ui';
 import Parallel from './Parallel.js'; 
+
 
 
 
 export default class Landing extends Component {
   constructor() {
     super();
-    this.state = {data: []}
+    this.state = {data: []};
+
+    this.setUpText = 'Encompass: it just makes sense. Don\'t think too hard about it.' +
+      ' Just look at our pretty graphs.';
   }
 
   // componentDidMount() {
@@ -20,10 +24,23 @@ export default class Landing extends Component {
   //   }.bind(this));
   // }
 
+    
   render() {
     return (
-      <div>  
-      <Parallel data={this.state.data} />
+      <div style={{width:'95%', margin: 'auto'}}>  
+      <h1>You Are Here</h1>
+      <Card>  
+        <CardHeader
+          title="Encompass"
+          subtitle="Insights"
+          avatar={<Avatar style={{color:'black'}}>E</Avatar>}/>
+        <CardText>
+          {this.setUpText}
+        </CardText>  
+        <div>  
+          <Parallel data={this.state.data} />
+        </div>
+      </Card> 
       </div>
     );
   }

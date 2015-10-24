e9d86f39d6982e3d248dfcbaba4de6fdcdbc574b
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-23 17:53:10 -0700

    Revise AddOffer and SearchCompany pages

--- server/frontend/components/SearchCompany.js
+++ server/frontend/components/SearchCompany.js
@@ -1,9 +1,20 @@
 import React, { PropTypes, Component } from 'react';
+import { Typeahead } from 'react-typeahead';
+
+import { CompanyNames } from '../constants';
 
 export default class SearchCompany extends Component {
   render() {
     return (
-      <div>Search Company Test</div>
+      <div>Company Name:
+	      <Typeahead 
+	        className="form-control" 
+	        name="company" 
+	        options={ CompanyNames } 
+	        placeholder="Uber" 
+	        maxVisible={10}
+	        />
+	  </div>
     );
   }
 }

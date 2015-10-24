c2cc892b7de12c2d4633014eae7fa34229b9583a
Benjamin <bhoffman8@gmail.com>
2015-10-23 19:47:45 -0700

    creates subviews for UserProfile page

---
+++ server/frontend/components/MyCompanies.js
@@ -0,0 +1,9 @@
+import React, { PropTypes, Component } from 'react';
+import _                               from 'lodash';
+
+export default class MyCompanies extends Component {
+
+  render () {
+    return (<div>FollowedCompanies Success!!!!!</div>)
+  }
+};
---
+++ server/frontend/components/MyOffers.js
@@ -0,0 +1,9 @@
+import React, { PropTypes, Component }  from 'react';
+import _                                from 'lodash';
+
+export default class MyOffers extends Component {
+
+  render () {
+    return (<div>MyOffers Success!!!!</div>);
+  }
+}

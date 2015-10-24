8a3a9145606fe9d9904ce1c1bb708a058e2a7457
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-23 19:47:28 -0700

    Continue draft of CompanySearch page

--- server/frontend/components/SearchCompany.js
+++ server/frontend/components/SearchCompany.js
@@ -2,18 +2,26 @@ import React, { PropTypes, Component } from 'react';
 import { Typeahead } from 'react-typeahead';
 
 import { CompanyNames } from '../constants';
+import CompanyProfile from './CompanyProfile';
 
 export default class SearchCompany extends Component {
+  constructor () {
+    super();
+    this.state = { data: {} };
+  }
+
   render() {
     return (
-      <div>Company Name:
-	      <Typeahead 
-	        className="form-control" 
-	        name="company" 
-	        options={ CompanyNames } 
-	        placeholder="Uber" 
-	        maxVisible={10}
-	        />
+      <div>
+      		Company Name:
+		     <Typeahead 
+		        className="form-control" 
+		        name="company" 
+		        options={ CompanyNames } 
+		        placeholder="Uber" 
+		        maxVisible={10}
+		        />
+		    <CompanyProfile />
 	  </div>
     );
   }

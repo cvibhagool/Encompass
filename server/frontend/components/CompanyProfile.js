7dc07e4875bb4bac356701840927b4aa63ffbbe9
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-26 16:41:59 -0700

    Style company search page

--- server/frontend/components/CompanyProfile.js
+++ server/frontend/components/CompanyProfile.js
@@ -1,6 +1,6 @@
 import React, { ProtoTypes, Component } from 'react';
 import _                                from 'lodash';
-
+import { Table, TableHeader, TableHeaderColumn, TableBody, TableRow, TableRowColumn } from 'material-ui';
 export default class CompanyProfile extends Component {
 
   constructor () {
@@ -20,13 +20,47 @@ export default class CompanyProfile extends Component {
   render() {
     const { apiData } = this.props;
     return (
-      <ul>
-        { apiData.employees && 
-          _.map(apiData, function(val, ind) {
-            return (<li key={ind}>{ind}: {val}</li>);
-          })
-        }
-      </ul>
+      
+      <div>
+      { apiData.employees &&
+        <Table>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}> 
+            <TableRow>
+              <TableHeaderColumn style={{textAlign: 'center'}} colSpan={2} key='name'> <h2>{apiData.name}</h2> </TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow>
+                <TableRowColumn>Company Website</TableRowColumn> <TableRowColumn key='website'>{apiData.website}</TableRowColumn>
+            </TableRow>
+            <TableRow>
+                <TableRowColumn>Total Employees</TableRowColumn> <TableRowColumn key='employees'>{apiData.employees.toLocaleString()}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>Founding Date</TableRowColumn> <TableRowColumn key='founding_date'>{apiData.founding_date ? apiData.founding_date.split('T')[0] : <i>No date on record</i>}</TableRowColumn>
+            </TableRow>
+            <TableRow> 
+                <TableRowColumn>Company Stage</TableRowColumn> <TableRowColumn key='stage'>{apiData.stage}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>Total Funding (USD)</TableRowColumn> <TableRowColumn key='total_funding'>${apiData.total_funding.toLocaleString()}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>Last Funding Date</TableRowColumn> <TableRowColumn key='last_funding_date'>{apiData.last_funding_date.split('T')[0]}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>City</TableRowColumn> <TableRowColumn key='city'>{apiData.city}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>State</TableRowColumn> <TableRowColumn key='state'>{apiData.state}</TableRowColumn>
+            </TableRow>
+            <TableRow>    
+                <TableRowColumn>Country</TableRowColumn> <TableRowColumn key='country'>{apiData.country}</TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
+      }
+      </div>
     )
   }
 };
--- server/frontend/components/SearchCompany.js
+++ server/frontend/components/SearchCompany.js
@@ -2,6 +2,8 @@ import React, { PropTypes, Component } from 'react';
 import { Typeahead } from 'react-typeahead';
 import _                                from 'lodash';
 
+import { Paper } from 'material-ui';
+
 import CompanyProfile from './CompanyProfile';
 
 export default class SearchCompany extends Component {
@@ -18,29 +20,36 @@ export default class SearchCompany extends Component {
   render() {
     return (
       <div>
-      		Company Name:
-          <Typeahead 
-            className="form-control" 
-            name="company" 
-            options={ this.state.companyNames } 
-            placeholder="Google" 
-            maxVisible={10}
-            onOptionSelected={
-              (name) =>  {
-                let companyEntry = _.find(this.props.companies, 'name', name);
-                this.setState({companyId: companyEntry.id});
+      		<h1>Search by Company Name:</h1>
+          <Paper className="left-pane" zDepth={1}>
+            <Typeahead 
+              name="company" 
+              options={ this.state.companyNames } 
+              placeholder="Google" 
+              maxVisible={10}
+              onOptionSelected={
+                (name) =>  {
+                  let companyEntry = _.find(this.props.companies, 'name', name);
+                  this.setState({companyId: companyEntry.id});
+                }
               }
-            }
-            customClasses={
-              { input: "typeahead-input",
-                results: "typeahead-results",
-                listItem: "typeahead-item",
-                hover: "typeahead-active"}
-            }
-            />
-        {this.state.companyId &&    
-          <CompanyProfile apiData={this.props.apiData} companyId={this.state.companyId} fetchApiData={this.props.fetchApiData}/>
-		    }
+              customClasses={
+                { input: "typeahead-input",
+                  results: "typeahead-results",
+                  listItem: "typeahead-item",
+                  hover: "typeahead-active"}
+              }
+              />
+            </Paper>
+          <Paper className="content-pane" zDepth={1}>
+            {this.state.companyId &&
+              <CompanyProfile 
+                className="content-pane"
+                apiData={this.props.apiData} 
+                companyId={this.state.companyId} 
+                fetchApiData={this.props.fetchApiData}/>
+    		    }
+          </Paper>
 	  </div>
     );
   }

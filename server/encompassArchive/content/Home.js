14aff27d70d612941591951c0e771ff0781320cb
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-27 15:18:49 -0700

    Delete deprecated components

--- server/encompassArchive/content/Home.js
+++
@@ -1,8 +0,0 @@
-// this is the main landing page; the homepage
-// should have info about our value proposition
-// cool graphs and charts to entice people to click
-// on click, navigate them to the login page
-
-// also include:
-// info about each of us
-// etc
--- server/encompassArchive/content/MyOffers.js
+++
@@ -1,2 +0,0 @@
-// this is a subview of the UserProfile page.
-// it will be a view of all the offers the user follows

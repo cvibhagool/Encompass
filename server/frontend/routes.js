2bcef0cdb78458cb0cd7c964c8a9b143c3d8f2e4
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-28 17:59:14 -0700

    Added a default child route to the landing page

--- server/frontend/routes.js
+++ server/frontend/routes.js
@@ -1,18 +1,20 @@
-import React 			      from 'react';
-import { Route } 		    from 'react-router';
-import App              from './containers/App';
-import Landing          from './components/Landing';
-import UserProfile     	from './components/UserProfile';
-import AddOffer        	from './components/AddOffer';
-import SearchCompany   	from './components/SearchCompany';
-import Login           	from './components/Login';
-import Signup          	from './components/Signup';
+import React 			            from 'react';
+//IndexRoute 
+import { IndexRoute, Route } 	from 'react-router';
+import App                    from './containers/App';
+import Landing                from './components/Landing';
+import UserProfile     	      from './components/UserProfile';
+import AddOffer        	      from './components/AddOffer';
+import SearchCompany   	      from './components/SearchCompany';
+import Login           	      from './components/Login';
+import Signup          	      from './components/Signup';
 
 export default (
   <Route 
   		path="/" 
   		component={App}
   >
+    <IndexRoute component={Landing} />   
   	<Route path="profile"
            component={UserProfile} />
     <Route path="addOffer"
@@ -22,6 +24,6 @@ export default (
     <Route path="login"
            component={Login} />
     <Route path="signup"
-           component={Signup} />   
+           component={Signup} />
   </Route>
 );

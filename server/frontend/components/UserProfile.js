6456f8e58980b28dd6411ddd6c8376df4d7ffb05
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-28 18:29:43 -0700

    Refactor UserProfile to check for profile in app state instead of cookie

--- server/frontend/components/UserProfile.js
+++ server/frontend/components/UserProfile.js
@@ -40,7 +40,7 @@ export default class UserProfile extends Component {
           <div>
 
           { /* check if user is logged in */ }
-          {this.checkCookie() ?
+          { profile ?
             
             <div>
 

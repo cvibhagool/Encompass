64267cf940d15663101a07a5ad631ca063eec2f1
Benjamin <bhoffman8@gmail.com>
2015-10-30 21:40:32 -0700

    Adds segment file

---
+++ server/frontend/components/Segment_.js
@@ -0,0 +1,12 @@
+import React        from 'react';
+// import segment  from 'segmentio-snippet';
+
+
+export default class Segment extends Component {
+  render() {
+    return null
+    {/*  return analytics.page() */}
+    
+  } 
+}
+

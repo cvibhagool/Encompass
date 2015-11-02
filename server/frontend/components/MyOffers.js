065d475f8ac4d9afe6b96f90823fdfb407417f84
Benjamin <bhoffman8@gmail.com>
2015-11-01 17:44:18 -0800

    Progress check to implement delete a co on MyCompanie

--- server/frontend/components/MyOffers.js
+++ server/frontend/components/MyOffers.js
@@ -25,7 +25,7 @@ export default class MyOffers extends Component {
   }
 
   // removes an offer from MyOffers after user clicks a row and 'delete' button
-  removeOffer(e) {
+  removeOffer() {
     var offer = this.props.apiData.offers[this.selectedRow[0]];
     this.props.removeApiData('/api/offer/', offer.id)
   }

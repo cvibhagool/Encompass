bec3c5c3385366d0730add4b01c11afe1505e5cd
Aaron Norby <aaron.norby@gmail.com>
2015-11-04 11:55:25 -0800

    Add tests for data api requests for industry graph and parallel coords graph

--- server/test/dataApiTests.js
+++ server/test/dataApiTests.js
@@ -20,4 +20,27 @@ describe('Data API', function() {
         done();
       })
   });
+
+  it('should get the industry graph request for search industry', function(done) {
+    request
+      .get('/data/company?fields[]=id&fields[]=name&fields[]=employees&fields[]=employees_mom&fields[]=total_funding&fields[]=stage&fields[]=founding_date&industry=search')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      })
+  });
+
+  it('should get the request for parallel coords graph', function(done) {
+    request
+      .get('/data/company?industry=all&fields[]=employees_mom&fields[]=employees&fields[]=total_funding')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      })
+  });
+
 });
